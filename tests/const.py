3d5a0b96595cf89b81bf59f064aa2da3f3b5e706
Alex Jung <jungdevelop@googlemail.com>
2024-12-10 23:59:01 +0100

    removed tests for now

--- tests/const.py
+++
@@ -1,9 +0,0 @@
-"""Constants for Public Transport Departures tests."""
-from custom_components.ha_departures.const import (
-    CONF_PASSWORD,
-)
-from custom_components.ha_departures.const import (
-    CONF_USERNAME,
-)
-
-MOCK_CONFIG = {CONF_USERNAME: "test_username", CONF_PASSWORD: "test_password"}
